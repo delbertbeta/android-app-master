cf390f7986ca55b8beae55ab9ada1a5a76e66b78
Hawkz <747036607@qq.com>
2018-09-22 11:18:24 +0800

    整合迭代1和汉堡菜单

--- app/src/main/java/com/lmmnb/guangzhouculturehelper/app/view/MainActivity.java
+++ app/src/main/java/com/lmmnb/guangzhouculturehelper/app/view/MainActivity.java
@@ -1,12 +1,19 @@
 package com.lmmnb.guangzhouculturehelper.app.view;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -17,8 +24,13 @@ import android.widget.Toast;
 import com.lmmnb.guangzhouculturehelper.R;
 import com.lmmnb.guangzhouculturehelper.api.apis.Language;
 import com.lmmnb.guangzhouculturehelper.api.apis.Test;
+import com.lmmnb.guangzhouculturehelper.app.view.Fragment.First_page;
+import com.lmmnb.guangzhouculturehelper.app.view.Fragment.First_page;
+import com.lmmnb.guangzhouculturehelper.app.view.Fragment.Welcome;
 import com.lmmnb.guangzhouculturehelper.app.view.widget.ExchangeButton;
+import com.lmmnb.guangzhouculturehelper.app.view.widget.MyAdapter;
 import com.lmmnb.guangzhouculturehelper.app.view.widget.MyEditText;
+import com.lmmnb.guangzhouculturehelper.app.view.widget.MyOption;
 import com.lmmnb.guangzhouculturehelper.app.view.widget.MyText;
 import com.lmmnb.guangzhouculturehelper.app.view.widget.RecordButton;
 import com.lmmnb.guangzhouculturehelper.app.view.widget.Ripple;
@@ -29,51 +41,68 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageButton clearbtn;   //清除按钮
-    private static ExchangeButton exchangbtn;
-    public  static MyEditText et1;
-    private RecordButton recordButton;  //录音按钮
+    private List<MyOption> list = new ArrayList<MyOption>();
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mActionBarDrawerToggle;
+    private ListView lvLeftMenu;
 
-    private ListView history;
-    //用于储存ListView中的MyText对象
-    private List<MyText> textList =new ArrayList<MyText>();
+    private Fragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.drawer);
 
-        //ListView部分
-        textInput();
-        TextAdapter adapter = new TextAdapter(MainActivity.this,R.layout.text_item,textList);
-        history = (ListView) findViewById(R.id.lv);
-        history.setAdapter(adapter);
+        fragment = new First_page();
+        getSupportFragmentManager().beginTransaction().replace(R.id.f1,fragment).commit();
 
-        et1=(MyEditText) findViewById(R.id.et1);
-
-        recordButton = (RecordButton)findViewById(R.id.record);
-        exchangbtn = (ExchangeButton)findViewById(R.id.ExchangeButton);
-
-
-        clearbtn=(ImageButton)findViewById(R.id.clearBtn);
-        clearbtn.setOnClickListener(new View.OnClickListener() {
+        init();
+        MyAdapter myAdapter = new MyAdapter(MainActivity.this,R.layout.option,list);
+        lvLeftMenu = (ListView) findViewById(R.id.lv_left_menu);
+        lvLeftMenu.setAdapter(myAdapter);
+        lvLeftMenu.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                et1.et2.setText("");
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                switch (i){
+                    case 0:
+                        mToolbar.setTitle("翻译助手");
+                        fragment = new First_page();
+                        mDrawerLayout.closeDrawer(Gravity.START);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.f1,fragment).commit();
+                        break;
+                    case 1:
+                        mToolbar.setTitle("美食助手");
+                        fragment = new Welcome();
+                        mDrawerLayout.closeDrawer(Gravity.START);
+                        getSupportFragmentManager().beginTransaction().replace(R.id.f1,fragment).commit();
+                        break;
+
+                }
             }
         });
+
+        mToolbar = (Toolbar) findViewById(R.id.tool_bar);
+        mToolbar.setTitle("广州文化助手");//customize the title,个性化设置title
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);//show back button and make it enabled
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.open, R.string.close);
+        mActionBarDrawerToggle.setDrawerIndicatorEnabled(true);
+        mActionBarDrawerToggle.setHomeAsUpIndicator(R.mipmap.ic_launcher);//channge the icon,改变图标
+        mActionBarDrawerToggle.syncState();////show the default icon and sync the DrawerToggle state,如果你想改变图标的话，这句话要去掉。这个会使用默认的三杠图标
+        mDrawerLayout.setDrawerListener(mActionBarDrawerToggle);//关联 drawerlayout
+
     }
 
-    private void textInput()
+    private void init()
     {
-        MyText a = new MyText("你好啊","雷猴啊");
-        textList.add(a);
-        MyText b = new MyText("不客气","唔该噻");
-        textList.add(b);
-        MyText c = new MyText("saonima","woaini");
-        textList.add(c);
-        MyText d = new MyText("woaini","saonima");
-        textList.add(d);
+        MyOption o1 = new MyOption("翻译",R.drawable.ic_translate_grey_700_24dp);
+        list.add(o1);
+        MyOption o2 = new MyOption("美食助手",R.drawable.ic_restaurant_grey_700_24dp);
+        list.add(o2);
     }
 
     private void translate()
@@ -89,14 +118,6 @@ public class MainActivity extends AppCompatActivity {
                 Log.i("demo", String.format("translate: %s -> %s", mandarin, cantonese));
             }
         }).start();
-    }
-
-    public static boolean getText()
-    {
-        if(exchangbtn.getStart().equals("普通话"))
-            return true;
-        else
-            return false;
     }
 
 
